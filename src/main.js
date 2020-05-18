a83e24c4571381120c42b9b54cf09987a85e1953
Teng Wei Song <weisong.teng.work@gmail.com>
2020-05-18 21:22:18 +0800

    added buefy

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueRouter from "vue-router";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(Buefy);
 
 new Vue({
   render: (h) => h(App),
