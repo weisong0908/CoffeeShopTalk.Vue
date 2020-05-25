5c44b7cbc87d1d9f4503a2ee0b604196bac771f3
Teng Wei Song <weisong.teng.work@gmail.com>
2020-05-25 20:04:30 +0800

    removed temp buttons from home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,6 @@
 <template>
     <page title="Coffee Shop Talk">
-        <b-button type="is-primary" tag="router-link" to="/chatroom">Go to chat</b-button>
-
-        <div v-if="!$auth.loading">
-            <b-button v-if="!$auth.isAuthenticated" @click="login">Log in</b-button>
-            <b-button v-if="$auth.isAuthenticated" @click="logout">Log out</b-button>
-        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </page>
 </template>
 
