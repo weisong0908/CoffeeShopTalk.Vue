3678862884cccc7b82c9bf9fc8dfd6599b876e63
Teng Wei Song <weisong.teng.work@gmail.com>
2020-05-25 11:40:32 +0800

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
     <div id="app">
-        <router-link to="/">Home</router-link>
-        <router-link to="/chatroom">Chatroom</router-link>
-        <router-link v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
-        <section class="section">
-            <div class="container">
-                <router-view></router-view>
-            </div>
-        </section>
+        <navbar :pages="pages"></navbar>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
+
 export default {
     name: "App",
-    components: {}
+    components: {
+        Navbar
+    },
+    data() {
+        return {
+            pages: [
+                { title: "Chatroom", path: "/chatroom" },
+                { title: "Profile", path: "/profile" }
+            ]
+        };
+    }
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item href="/">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Coffee Shop Talk"
+                />
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item
+                tag="router-link"
+                v-for="page in pages"
+                :key="page.title"
+                :to="page.path"
+            >{{ page.title }}</b-navbar-item>
+        </template>
+        <template slot="end">
+            <b-navbar-dropdown hoverable>
+                <div slot="label">
+                    <p v-if="$auth.isAuthenticated">Hi, {{ $auth.user.nickname }}!</p>
+                    <p v-else>Account</p>
+                </div>
+                <div v-if="$auth.isAuthenticated">
+                    <b-navbar-item tag="router-link" to="/profile">Profile</b-navbar-item>
+                    <b-navbar-item @click="logout">Log out</b-navbar-item>
+                </div>
+                <div v-else>
+                    <b-navbar-item @click="login">Log in</b-navbar-item>
+                </div>
+            </b-navbar-dropdown>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    props: ["pages"],
+    methods: {
+        login() {
+            this.$auth.loginWithRedirect();
+        },
+        logout() {
+            this.$auth.logout({
+                returnTo: window.location.origin
+            });
+        }
+    }
+};
+</script>
