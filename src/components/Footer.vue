fe693783011c0976cb2ca6b90e18371c0ac1e265
Teng Wei Song <weisong.teng.work@gmail.com>
2020-05-25 22:20:29 +0800

    updated footer content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,47 +4,44 @@
             Coffee Shop Talk
             <small class="has-text-grey">&nbsp; {{ appVersion }}</small>
         </p>
-        <p class="subtitle is-6">A project by Teng Wei Song</p>
+        <p class="subtitle is-6">A project by <a href="https://tengweisong.com" target="_blank" rel="noopener noreferrer">Teng Wei Song</a></p>
+        <hr/>
         <div class="columns">
             <div class="column is-one-third">
-                <p>This is a simple real-time chat application</p>
+                <p class="title is-4">What is this project?</p>
+                <p>
+                    <strong>Coffee Shop Talk</strong> is a simple real-time text-based chat application. The client- and server-side components are developed separately to ensure loosely coupled architecture.
+                </p>
                 <br />
-                <b-taglist attached>
-                    <b-tag type="is-black" size="is-medium">
-                        <a
-                            href="https://github.com/weisong0908/CoffeeShopTalk.Vue"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <b-icon type="is-light" icon="github-circle"></b-icon>
-                        </a>
-                    </b-tag>
-                    <b-tag type="is-dark" size="is-medium">Github repository</b-tag>
-                </b-taglist>
-                <b-taglist attached>
-                    <b-tag type="is-black" size="is-medium">
-                        <a href="https://tengweisong.com" target="_blank" rel="noopener noreferrer">
-                            <b-icon type="is-light" icon="wordpress"></b-icon>
-                        </a>
-                    </b-tag>
-                    <b-tag type="is-dark" size="is-medium">Personal blog</b-tag>
-                </b-taglist>
+                <p>This project makes use of <strong>Github Actions</strong> to achieve CICD (continous integrtion and continous deployment) which ensures latest developemnt is deployed and running immediately.</p>
+                <br />
+                <div class="buttons">
+                    <b-button
+                        tag="a"
+                        size="is-small"
+                        href="https://github.com/weisong0908/CoffeeShopTalk.Vue"
+                        target="_blank"
+                        icon-left="github-circle"
+                    >Frontend repository</b-button>
+                    <b-button
+                        tag="a"
+                        size="is-small"
+                        href="https://github.com/weisong0908/CoffeeShopTalk.WebApi"
+                        target="_blank"
+                        icon-left="github-circle"
+                    >Backend repository</b-button>
+                </div>
             </div>
             <div class="column is-one-third">
-                <p class="subtitle is-6">Developed with:</p>
-                <p>
-                    <strong>ASP.NET Core 3.1</strong>
-                </p>
-                <small>Web APIs and SignalR are used to make real-time communication possible, thanks to the .NET Core technology.</small>
+                <p class="title is-4">Technology stack</p>
+                <p class="title is-6">ASP.NET Core 3.1</p>
+                <p>Web APIs and SignalR are used to make real-time communication possible, thanks to the .NET Core technology.</p>
                 <br />
-                <br />
-                <p>
-                    <strong>Vue 2.6</strong>
-                </p>
-                <small>Vue helps building the frontend as a single page application.</small>
+                <p class="title is-6">Vue 2.6</p>
+                <p>Vue helps building the frontend as a single page application.</p>
             </div>
             <div class="column is-one-third">
-                <p class="subtitle is-6">Attributes</p>
+                <p class="title is-4">Attributes</p>
                 <p>
                     Several icons used in this application are made by
                     <a
