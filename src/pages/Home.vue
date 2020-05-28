6ee3793724d85f1e462ca6634dfcbf5004b645cd
Teng Wei Song <weisong.teng.work@gmail.com>
2020-05-28 22:37:03 +0800

    added test for access token

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,22 @@
 <template>
     <page title="Coffee Shop Talk">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <b-button @click="getWeather">Get weather</b-button>
+        <p>{{ weather }}</p>
     </page>
 </template>
 
 <script>
 import Page from "../components/Page";
+import axios from "axios";
 
 export default {
     components: { Page },
+    data() {
+        return {
+            weather: ""
+        };
+    },
     methods: {
         login() {
             this.$auth.loginWithRedirect();
@@ -17,6 +25,20 @@ export default {
             this.$auth.logout({
                 returnTo: window.location.origin
             });
+        },
+        async getWeather() {
+            const token = await this.$auth.getTokenSilently();
+
+            const { data } = await axios.get(
+                "https://localhost:5001/WeatherForecast",
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+
+            this.weather = data;
         }
     }
 };
